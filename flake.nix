01ae43c4b674a486eb9a69a4c47aa5126be6cf09
psiana011 <psiana011.endo@gmail.com>
2025-07-15 11:32:32 +0900

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.stable.latest.minimal;
+          rustc = pkgs.rust-bin.stable.latest.minimal;
+        };
+
+        pyrefly = rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "pyrefly";
+          version = "0.24.1";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "facebook";
+            repo = "pyrefly";
+            tag = finalAttrs.version;
+            hash = "sha256-P4qZ8/UCNm35EbwxD0u23auPuJwX0XjnBxRjD6Qf67c=";
+          };
+
+          buildAndTestSubdir = "pyrefly";
+          cargoHash = "sha256-zHSoDlXC7uIVwgXPyUO27H3B0a6yWU+2O09gQC9cv2o=";
+
+          nativeInstallCheckInputs = [pkgs.versionCheckHook];
+          doInstallCheck = true;
+
+          # requires unstable rust features
+          env.RUSTC_BOOTSTRAP = 1;
+
+          # passthru.updateScript = nix-update-script {};
+
+          meta = {
+            description = "Fast type checker and IDE for Python";
+            homepage = "https://github.com/facebook/pyrefly";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "pyrefly";
+            platforms = pkgs.lib.platforms.linux ++ pkgs.lib.platforms.darwin;
+            maintainers = with pkgs.lib.maintainers; [
+              cybardev
+              QuiNzX
+            ];
+          };
+        });
+      in {
+        packages = {
+          inherit pyrefly;
+          default = pyrefly;
+        };
+      }
+    );
+}
