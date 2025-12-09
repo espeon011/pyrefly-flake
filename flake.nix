19e069109f9354d911c532d87d83f131e400ce2a
psiana011 <psiana011.endo@gmail.com>
2025-12-10 01:27:56 +0900

    update to v0.45.0

--- flake.nix
+++ flake.nix
@@ -26,24 +26,24 @@
 
         pyrefly = rustPlatform.buildRustPackage (finalAttrs: {
           pname = "pyrefly";
-          version = "0.44.1";
+          version = "0.45.0";
 
           src = pkgs.fetchFromGitHub {
             owner = "facebook";
             repo = "pyrefly";
             tag = finalAttrs.version;
-            hash = "sha256-oCdc7HWNc7dZdM1zazNh+OibPN0BwADk2nY9n2U9TnU=";
+            hash = "sha256-G1gE1KoAQnpuwRvcZ/W7JOzPQYi5A/x5PKHuKM50FXM=";
           };
 
           buildAndTestSubdir = "pyrefly";
-          cargoHash = "sha256-eabLdzo190C3qO4kSf7nbbLtG1CkvupuDSP0KOIAxFc=";
+          cargoHash = "sha256-VuASsxjSt7qAfJ5kwkC293KzypipJGEvhOOyQusge/Q=";
 
           nativeInstallCheckInputs = [pkgs.versionCheckHook];
           doInstallCheck = true;
           checkFlags = [
+            "--skip=test::lsp::lsp_interaction::configuration::test_interpreter_change_removes_type_errors"
             "--skip=test::lsp::lsp_interaction::configuration::test_pythonpath_change"
             "--skip=test::lsp::lsp_interaction::configuration::test_workspace_pythonpath_ignored_when_set_in_config_file"
-            "--skip=test::lsp::lsp_interaction::notebook_definition::test_notebook_definition_import"
           ];
 
           # requires unstable rust features
